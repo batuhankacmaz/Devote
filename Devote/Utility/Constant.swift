64000d2b4468c5c1e7bfad9344db302b164a4323
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2023-03-05 13:26:28 +0300

    refactor core data model

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Batuhan Kacmaz on 5.03.2023.
+//
+
+import Foundation
+
+
+//MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+
+//MARK: - UI
+
+//MARK: - UX

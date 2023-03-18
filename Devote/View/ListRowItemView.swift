b57623bc07b26bec4529353bd9d559efad06179b
Batuhan Kacmaz <batuhan.kacmaz1105@gmail.com>
2023-03-18 12:56:36 +0300

    create custom checkbox and listRowItem

---
+++ Devote/View/ListRowItemView.swift
@@ -0,0 +1,32 @@
+//
+//  ListRowItemView.swift
+//  Devote
+//
+//  Created by Batuhan Kacmaz on 18.03.2023.
+//
+
+import SwiftUI
+
+struct ListRowItemView: View {
+    @Environment(\.managedObjectContext) var viewContext
+    @ObservedObject var item: Item
+    
+    var body: some View {
+        Toggle(isOn: $item.completion) {
+            Text(item.task ?? "")
+                .font(.system(.title2, design: .rounded))
+                .fontWeight(.heavy)
+                .foregroundColor(item.completion ? Color.pink : Color.primary)
+                .padding(.vertical, 12)
+                .animation(.default, value: item.completion)
+        } //: TOGGLE
+        .toggleStyle(CheckBoxStyle())
+        .onReceive(item.objectWillChange) { _ in
+            if self.viewContext.hasChanges {
+                try? self.viewContext.save()
+            }
+        }
+    }
+}
+
+
